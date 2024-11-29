a7bc802e353d03df94bef065b2548960563de216
AlexanderZhang(CodingRookieA) <zhangalexander6@gmail.com>
2024-11-28 22:38:14 -0500

    feat: Added part where users input their countries

--- ProjectStarter/app/src/main/java/com/example/myapplication/intro.java
+++ ProjectStarter/app/src/main/java/com/example/myapplication/intro.java
@@ -4,29 +4,46 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.IOException;
+import java.util.List;
 
 public class intro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FirebaseAuth database = FirebaseAuth.getInstance();
+        String userId = database.getCurrentUser().getUid();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users").child(userId);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_intro);
         Button next = findViewById(R.id.next);
+        EditText input = findViewById(R.id.Input);
+        List<String> countryList = Search.readFileFromAssets(this, "Countries.txt");
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(intro.this, q1.class);
-                startActivity(intent);
+            String count = input.getText().toString().trim().toLowerCase();
+            if(countryList.contains(count)){
+                ref.child("Country").setValue(count);
+                Intent i = new Intent(intro.this, q1.class);
+                startActivity(i);
+            }else{
+                Toast.makeText(intro.this, "Invalid input, try again please", Toast.LENGTH_SHORT).show();
+            }
             }
         });
+
 
     }
 }
